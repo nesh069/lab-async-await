9d08aad774b8f0d21f6c535e7c772f4bb0157aad
Emmanuel Munene <emmanuel.munene@student.moringaschool.com>
2026-04-15 14:48:12 +0300

    lab assesmnet

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 // Write your code here!
+const posts = [
+  {
+    title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  }
+];
+
 async function getPosts() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-  const posts = await response.json();
-  displayPosts(posts);
+  const data = await response.json();
+  displayPosts(data);
 }
 
 function displayPosts(posts) {
@@ -21,5 +28,7 @@ function displayPosts(posts) {
     ul.appendChild(li);
   });
 }
+
+displayPosts(posts);
 
 getPosts();
